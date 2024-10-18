5f5571ab4011eb2a8110ca54b572ed5ee02f885e
Brett Shollenberger <brett.shollenberger@gmail.com>
2024-10-18 12:40:09 -0400

    0.1.5

--- lib/easy_ml.rb
+++ lib/easy_ml.rb
@@ -13,10 +13,13 @@ require_relative "easy_ml/engine"
 module EasyML
   class Error < StandardError; end
 
-  require_relative "easy_ml/support"
-  require_relative "easy_ml/core_ext"
-  require_relative "easy_ml/logging"
-  require_relative "easy_ml/data"
-  require_relative "easy_ml/transforms"
-  require_relative "easy_ml/core"
+  # Skip if running migration generators
+  unless defined?(Rails::Generators) && Rails.const_defined?("Generators")
+    require_relative "easy_ml/support"
+    require_relative "easy_ml/core_ext"
+    require_relative "easy_ml/logging"
+    require_relative "easy_ml/data"
+    require_relative "easy_ml/transforms"
+    require_relative "easy_ml/core"
+  end
 end

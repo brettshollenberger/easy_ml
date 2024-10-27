1f91635944ce59edf249afb0f58c4f66745de284
Brett Shollenberger <brett.shollenberger@gmail.com>
2024-10-27 10:22:38 -0400

    Parquet  reader

--- spec/support/file_spec_helper.rb
+++ spec/support/file_spec_helper.rb
@@ -2,14 +2,14 @@ module FileSpecHelper
   def file_spec
     path = SPEC_ROOT.join("lib/easy_ml/data/dataset/data/files/raw")
     csv_path = path.join("file.csv")
-    parquet_path = path.join("file.parquet")
+    parquet_path = path.join("parquet/file.parquet")
     restore_path = SPEC_ROOT.join("file.csv")
 
     FileUtils.cp(csv_path, restore_path)
 
     yield path, restore_path, parquet_path
   ensure
-    FileUtils.rm(path.join("file.parquet")) if File.exist?(path.join("file.parquet"))
+    FileUtils.rm(parquet_path) if File.exist?(parquet_path)
     FileUtils.mv(restore_path, csv_path)
   end
 end

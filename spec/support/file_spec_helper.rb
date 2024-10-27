cbeef25dba7dd2edb75f5f349b7b4d3fc244265b
Brett Shollenberger <brett.shollenberger@gmail.com>
2024-10-27 10:10:19 -0400

    Fixing

--- spec/support/file_spec_helper.rb
+++ spec/support/file_spec_helper.rb
@@ -1,14 +1,15 @@
 module FileSpecHelper
   def file_spec
     path = SPEC_ROOT.join("lib/easy_ml/data/dataset/data/files/raw")
-    csv_file = path.join("file.csv")
-    parquet_file = path.join("file.parquet")
+    csv_path = path.join("file.csv")
+    parquet_path = path.join("file.parquet")
+    restore_path = SPEC_ROOT.join("file.csv")
 
-    FileUtils.cp(csv_file, SPEC_ROOT.join("file.csv")) # This gets wiped by PolarsReader
+    FileUtils.cp(csv_path, restore_path)
 
-    yield csv_file, parquet_file
+    yield path, restore_path, parquet_path
   ensure
     FileUtils.rm(path.join("file.parquet")) if File.exist?(path.join("file.parquet"))
-    FileUtils.mv(SPEC_ROOT.join("file.csv"), csv_file)
+    FileUtils.mv(restore_path, csv_path)
   end
 end

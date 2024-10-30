d3c88baa83602596be54c993479aa22a0f6b01a9
Brett Shollenberger <brett.shollenberger@gmail.com>
2024-10-30 11:57:45 -0400

    Add XGBoost Hyperparameters

--- spec/support/file_spec_helper.rb
+++ spec/support/file_spec_helper.rb
@@ -1,15 +1,20 @@
+require "thread"
 module FileSpecHelper
+  MUTEX = Mutex.new
+
   def file_spec
-    path = SPEC_ROOT.join("lib/easy_ml/data/dataset/data/files/raw")
-    csv_path = path.join("file.csv")
-    parquet_path = path.join("parquet/file.parquet")
-    restore_path = SPEC_ROOT.join("file.csv")
+    MUTEX.synchronize do
+      path = SPEC_ROOT.join("lib/easy_ml/data/dataset/data/files/raw")
+      csv_path = path.join("file.csv")
+      parquet_path = path.join("parquet/file.parquet")
+      restore_path = SPEC_ROOT.join("file.csv")
 
-    FileUtils.cp(csv_path, restore_path)
+      FileUtils.cp(csv_path, restore_path)
 
-    yield path, restore_path, parquet_path
-  ensure
-    FileUtils.rm(parquet_path) if File.exist?(parquet_path)
-    FileUtils.mv(restore_path, csv_path)
+      yield path, restore_path, parquet_path
+    ensure
+      FileUtils.rm(parquet_path) if File.exist?(parquet_path)
+      FileUtils.mv(restore_path, csv_path)
+    end
   end
 end

9b2fac00ad0809b02a3af12a72e9225c21b7204e
Brett Shollenberger <brett.shollenberger@gmail.com>
2025-01-08 13:21:31 -0500

    Fixing resque pool

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,10 +1,3 @@
 require "resque"
-require "resque-pool"
-
-gem_path = Gem::Specification.find_by_name("easy_ml").gem_dir
-Resque::Pool.configure do |config|
-  config.path = File.join(gem_path, "config", "resque-pool.yml")
-  puts "Resque pool config: #{config.path}"
-end
 
 Resque.redis = ENV["REDIS_URL"] || "redis://localhost:6379"
--- lib/easy_ml/version.rb
+++ lib/easy_ml/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module EasyML
-  VERSION = "0.2.0-rc12"
+  VERSION = "0.2.0-rc13"
 
   module Version
   end

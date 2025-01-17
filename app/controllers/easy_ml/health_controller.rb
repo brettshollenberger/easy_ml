3c8a559057d35660789a6293e33ab7591c87d052
Brett Shollenberger <brett.shollenberger@gmail.com>
2025-01-17 14:55:38 -0500

    0.2.0.pre.rc26

--- app/controllers/easy_ml/health_controller.rb
+++ app/controllers/easy_ml/health_controller.rb
@@ -2,7 +2,6 @@ module EasyML
   class HealthController < ApplicationController
     # No authentication or CSRF checks for this action
     skip_before_action :verify_authenticity_token
-    skip_before_action :force_ssl
 
     def up
       render json: { status: "OK" }, status: :ok
--- lib/easy_ml/version.rb
+++ lib/easy_ml/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module EasyML
-  VERSION = "0.2.0-rc25"
+  VERSION = "0.2.0-rc26"
 
   module Version
   end

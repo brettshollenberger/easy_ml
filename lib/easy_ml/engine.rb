098d0099d725d34ac2a462f91441d2228d2dc5c0
Brett Shollenberger <brett.shollenberger@gmail.com>
2025-01-28 16:48:49 -0500

    Remove access keys from the UI, pass in via AWS Secrets

--- lib/easy_ml/engine.rb
+++ lib/easy_ml/engine.rb
@@ -88,6 +88,17 @@ module EasyML
       end
     end
 
+    initializer "easy_ml.configure_secrets" do
+      EasyML::Configuration.configure do |config|
+        raise "S3_ACCESS_KEY_ID is missing. Set ENV['S3_ACCESS_KEY_ID']" unless ENV["S3_ACCESS_KEY_ID"]
+        raise "S3_SECRET_ACCESS_KEY is missing. Set ENV['S3_SECRET_ACCESS_KEY']" unless ENV["S3_SECRET_ACCESS_KEY"]
+
+        config.s3_access_key_id = ENV["S3_ACCESS_KEY_ID"]
+        config.s3_secret_access_key = ENV["S3_SECRET_ACCESS_KEY"]
+        config.wandb_api_key = ENV["WANDB_API_KEY"] if ENV["WANDB_API_KEY"]
+      end
+    end
+
     initializer "easy_ml.setup_generators" do |app|
       generators_path = EasyML::Engine.root.join("lib/easy_ml/railtie/generators")
       generators_dirs = Dir[File.join(generators_path, "**", "*.rb")]
--- lib/easy_ml/version.rb
+++ lib/easy_ml/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module EasyML
-  VERSION = "0.2.0-rc42"
+  VERSION = "0.2.0-rc43"
 
   module Version
   end

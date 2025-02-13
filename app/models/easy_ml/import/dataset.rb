1e86c8b81bee1c043a77ca2521d752f907e8e239
Brett Shollenberger <brett.shollenberger@gmail.com>
2025-02-12 20:41:33 -0500

    Fix import dataset

--- app/models/easy_ml/import/dataset.rb
+++ app/models/easy_ml/import/dataset.rb
@@ -13,9 +13,11 @@ module EasyML
         columns_config = dataset_config.delete("columns") || []
         features_config = dataset_config.delete("features") || []
 
-        name = dataset_config["name"]
-        dataset = EasyML::Dataset.find_by(name: name)
-        action = dataset.present? ? :update : :create
+        if action == :create
+          name = dataset_config["name"]
+          dataset = EasyML::Dataset.find_by(name: name)
+          action = dataset.present? ? :update : :create
+        end
         raise ArgumentError, "Action must be specified" unless action.present?
 
         if action == :create

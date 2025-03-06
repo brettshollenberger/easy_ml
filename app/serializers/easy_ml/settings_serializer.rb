1e42e3ad6fe3005d404d75c40f23617d8be75e9e
Brett Shollenberger <brett.shollenberger@gmail.com>
2025-03-06 09:15:24 -0500

    Add rc94

--- app/serializers/easy_ml/settings_serializer.rb
+++ app/serializers/easy_ml/settings_serializer.rb
@@ -5,5 +5,21 @@ module EasyML
     include JSONAPI::Serializer
 
     attributes *EasyML::Settings.configuration_attributes
+
+    attribute :version do |object|
+      EasyML::VERSION
+    end
+
+    attribute :git_sha do |object|
+      # Get git SHA of the main app
+      if Rails.root.join('.git').exist?
+        sha = `cd #{Rails.root} && git rev-parse HEAD`.strip
+        sha.presence || "Git SHA unavailable"
+      else
+        "Not a git repository"
+      end
+    rescue
+      "Error determining git SHA"
+    end
   end
 end
